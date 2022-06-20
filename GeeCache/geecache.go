42778e1c4e339708a984678408c7809d41f4bfb6
Daydreumer <2765129970@qq.com>
2022-06-20 19:51:21 +0800

    finish the GeeCache and add the test of Get case.

--- GeeCache/geecache.go
+++ GeeCache/geecache.go
@@ -1,5 +1,11 @@
 package GeeCache
 
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
 type Getter interface {
 	Get(key string) ([]byte, error)
 }
@@ -8,4 +14,71 @@ type GetterFunc func(key string) ([]byte, error)
 
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	// p1: 从mainCache中查询缓存
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	// p2: 缓存不存在，去节点本地查找
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	// p3: 调用回调函数getter.Get()函数获取源数据，并加入缓存mainCache中
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneByte(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
