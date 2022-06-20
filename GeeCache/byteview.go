4289ba4580bad5102233cb9476be73a52b5b5e79
Daydreumer <2765129970@qq.com>
2022-06-20 17:09:24 +0800

    add the geecache and test cases

---
+++ GeeCache/byteview.go
@@ -0,0 +1,23 @@
+package GeeCache
+
+type ByteView struct {
+	b []byte
+}
+
+func (bv ByteView) len() int {
+	return len(bv.b)
+}
+
+func (bv ByteView) ByteSlice() []byte {
+	return cloneByte(bv.b)
+}
+
+func (bv ByteView) String() string {
+	return string(bv.b)
+}
+
+func cloneByte(b []byte) []byte {
+	cp := make([]byte, len(b))
+	copy(cp, b)
+	return cp
+}
